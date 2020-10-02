00993edf24670bd3781a15b1337fb1b3f13f8a3c
RyuSA <kommsussertad@gmail.com>
2020-10-02 09:24:39 +0900

    init

---
+++ src/main/java/com/github/ryusa/stackoverflow/springcannotconnecttoignite/SpringCannotConnectToIgniteApplication.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.stackoverflow.springcannotconnecttoignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCannotConnectToIgniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCannotConnectToIgniteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ryusa/stackoverflow/springcannotconnecttoignite/SpringCannotConnectToIgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.stackoverflow.springcannotconnecttoignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCannotConnectToIgniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
