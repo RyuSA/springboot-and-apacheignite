916d275b989a5f8686a16590e7959b8b9d5025aa
RyuSA <kommsussertad@gmail.com>
2020-10-02 00:54:27 +0000

    add debug endpoint

--- src/main/java/com/github/ryusa/stackoverflow/springcannotconnecttoignite/SpringCannotConnectToIgniteApplication.java
+++ src/main/java/com/github/ryusa/stackoverflow/springcannotconnecttoignite/SpringCannotConnectToIgniteApplication.java
@@ -1,13 +1,30 @@
 package com.github.ryusa.stackoverflow.springcannotconnecttoignite;
 
+import org.apache.ignite.Ignite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
 public class SpringCannotConnectToIgniteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCannotConnectToIgniteApplication.class, args);
+	}
+
+	@RequiredArgsConstructor
+	@RestController
+	public static class Debug {
+
+		private final Ignite igniteInstance;
+
+		@GetMapping(path = "/nodenumber")
+		public int node_number() {
+			return this.igniteInstance.cluster().nodes().size();
+		}
 	}
 
 }
